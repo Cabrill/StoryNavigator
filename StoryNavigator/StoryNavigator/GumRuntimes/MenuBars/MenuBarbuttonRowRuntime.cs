f096eec03d1d351a6c6f1f77121772a3956e4cc8
Jared Bagley <cabrill@gmail.com>
2019-12-06 21:34:12 -0800

    Added the base class and a few Gum components for displaying a node and a menu

---
+++ StoryNavigator/StoryNavigator/GumRuntimes/MenuBars/MenuBarbuttonRowRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StoryNavigator.GumRuntimes.MenuBars
+{
+    public partial class MenuBarbuttonRowRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
